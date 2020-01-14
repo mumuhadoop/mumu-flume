884ce6d194b06a1a65de38631b77572c274b3730
babymm <babymm@aliyun.com>
2020-01-14 17:13:52 +0800

    添加本地文件到kafka数据集成。

--- src/main/resources/flume/start.sh
+++ src/main/resources/flume/start.sh
@@ -10,4 +10,5 @@ if [ $FLUME_HOME == null ];then
     echo "please set FLUME_HOME env";
   exit 1;
 fi
+# nohup flume-ng agent --conf-file flume-kafka.properties --name dpi-agent --conf /usr/local/apache-flume-1.9.0-bin/conf  &
 sh flume-ng agent --conf-file $1 --name $2 --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console
